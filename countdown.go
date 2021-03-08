a25c005afa50c64e926d4cfae7a13d26aea7f5f0
Jaume Jiménez <jaume.jimenezforteza@metro-markets.de>
2021-03-08 12:03:52 +0100

    V2: Implementation & test works, test no longer lasts 4s, but it doesn't check the wait-write sequence

--- countdown.go
+++ countdown.go
@@ -10,15 +10,26 @@ import (
 const finalWord = "Go!"
 const countDownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (s *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countDownStart; i > 0; i-- {
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 		fmt.Fprintln(out, i)
 	}
-	time.Sleep(1 * time.Second)
+	sleeper.Sleep()
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
--- countdown_test.go
+++ countdown_test.go
@@ -5,14 +5,27 @@ import (
 	"testing"
 )
 
+type spySleeper struct {
+	Calls int
+}
+
+func (s *spySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
-	Countdown(buffer)
+	sleeper := &spySleeper{}
+	Countdown(buffer, sleeper)
 
 	want := "3\n2\n1\nGo!"
 	got := buffer.String()
 
 	if got != want {
 		t.Errorf("got %s want %s", got, want)
+	}
+
+	if sleeper.Calls != 4 {
+		t.Errorf("got %d want 4 sleeper calls", sleeper.Calls)
 	}
 }
